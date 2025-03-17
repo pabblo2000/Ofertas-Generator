7e41a29905593dcf9adb81a7315d685643fcb68c
Pablo Álvaro <palvaroh2000@gmail.com>
2025-03-17 17:39:22 +0100

    Add Python installation check and virtual environment setup to installer script

--- installer.ps1
+++ installer.ps1
@@ -1,8 +1,26 @@
-# installer.ps1
-
 $OutputEncoding = [System.Text.Encoding]::UTF8
 Write-Host "Iniciando instalacion..."
 
+# Comprobar si Python está instalado
+if (-not (Get-Command python -ErrorAction SilentlyContinue)) {
+    Write-Host "Python no se encuentra instalado. Se procederá a descargar e instalar Python 3.11.9..."
+    $pythonInstallerUrl = "https://www.python.org/ftp/python/3.11.9/python-3.11.9-amd64.exe"
+    $installerPath = "$env:TEMP\python-3.11.9-amd64.exe"
+    Invoke-WebRequest -Uri $pythonInstallerUrl -OutFile $installerPath
+    Write-Host "Instalando Python 3.11.9..."
+    Start-Process -FilePath $installerPath -ArgumentList "/quiet InstallAllUsers=1 PrependPath=1" -Wait
+    Remove-Item $installerPath
+    if (-not (Get-Command python -ErrorAction SilentlyContinue)) {
+        Write-Host "ERROR: No se pudo instalar Python 3.11.9. Por favor, instale Python manualmente."
+        exit 1
+    } else {
+        Write-Host "Python 3.11.9 instalado correctamente."
+    }
+} else {
+    Write-Host "Python ya está instalado."
+}
+
+# Verificar si existe el entorno virtual (.venv) y crearlo si no existe
 if (-not (Test-Path ".venv")) {
     Write-Host "Entorno virtual no encontrado. Creando .venv..."
     python -m venv .venv
@@ -31,8 +49,6 @@ python -m pip install -r requirements.txt
 Write-Host "*******************************************************"
 Write-Host "**** APLICACION INSTALADA ****"
 Write-Host "*******************************************************"
-Write-Host "**** EJECUTE run_app.vbs PARA INICIAR LA APLICACION****"
-Write-Host "*******************************************************"
-Write-Host "**** PUEDE CERRAR ESTA VENTANA ****"
+Write-Host "**** EJECUTE run_app.vbs PARA INICIAR LA APLICACION ****"
 Write-Host "*******************************************************"
 Pause
--- run_app.vbs
+++ run_app.vbs
@@ -1,3 +1,4 @@
 Set WshShell = CreateObject("WScript.Shell")
+' Ejecuta run_app.ps1 de forma oculta (0 para ventana oculta)
 WshShell.Run "powershell -ExecutionPolicy Bypass -File """ & CreateObject("Scripting.FileSystemObject").GetParentFolderName(WScript.ScriptFullName) & "\run_app.ps1""", 0
 Set WshShell = Nothing
